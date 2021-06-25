cd691edd410a0a388ea6d04ea21fa7a4b0b909c1
JinHwanJung <yadot1478@naver.com>
2021-06-26 00:43:21 +0900

    model field 탐구중

--- study/models.py
+++ study/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class MyModel(models.Model):
-    pass
+    boolean_field = models.BooleanField(default=False)
+    char_field = models.CharField(max_length=50, null=True)
+    integer_field = models.IntegerField(null=True)
+    date_field = models.DateField(null=True)
+    datetime_field = models.DateTimeField(null=True)
+    # o_field = models.Field(null=True)
