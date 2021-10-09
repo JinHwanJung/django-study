7d4ec5c332daa187f54467af2d9e295ce00624c1
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2021-10-10 03:57:09 +0900

    feat(test_app/config/views.py, urls.py): index view

--- test_app/config/urls.py
+++ test_app/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from config.views import index
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 ]
---
+++ test_app/config/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    content = """
+    <html>
+    <head>
+        <title>Django Homepage</title>
+    </head>
+    <body>
+        <h1>안녕</h1>
+    </body>
+    </html>
+    """
+    response = HttpResponse(content)
+    response.write('<h2>Django Hello<h2>')
+    response['Custom-Header'] = 'Custom Header Value'
+    return response
