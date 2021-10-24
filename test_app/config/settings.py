0e272d296be5be917959d8bbb854ba20dca4e0e9
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2021-10-24 11:45:00 +0900

    update settings.py

--- test_app/config/settings.py
+++ test_app/config/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'base.middleware.URLMiddleware'
+    'base.middleware.RoutingMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
